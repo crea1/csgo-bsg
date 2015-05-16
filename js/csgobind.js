eab81d39aab15ee6d22034407c6d74778488dece
Marius Kristensen <marius.kristensen@gmail.com>
2015-05-17 01:29:24 +0200

    Primary and secondary now works

--- js/csgobind.js
+++ js/csgobind.js
@@ -1,11 +1,16 @@
 var app = angular.module('csgoBindApp', []);
     app.controller('bindController', ['$scope', '$http',
         function($scope, $http) {
+
+            $scope.selectedBindnames = [];
+            $scope.buyScript = "";
+
             $http.get('csgoitems.json').success(function(data) {
                 $scope.primary = [];
                 $scope.secondary = [];
                 $scope.grenades = [];
                 $scope.equipment = [];
+
                 angular.forEach(data.secondary, function(secondary) {
                     $scope.secondary.push(secondary);
                 });
@@ -20,19 +25,24 @@ var app = angular.module('csgoBindApp', []);
                 });
             });
 
+            $scope.selectAddToScript = function(newBind, oldBind) {
+                console.log(newBind + " " + oldBind);
+                $scope.addToScript(oldBind, false);
+                $scope.addToScript(newBind, true);
+                console.log($scope.selectedBindnames);
+            };
 
-            $scope.selectedBindnames = [];
             $scope.addToScript = function(bindname, selected) {
                 if (selected ) {
                     $scope.selectedBindnames.push(bindname);
-                } else {
+                } else if (!selected && bindname){
+                    console.log("remove " + bindname);
                     var index = $scope.selectedBindnames.indexOf(bindname);
                     $scope.selectedBindnames.splice(index, 1);
-
                 }
                 $scope.generateScript();
             };
-            $scope.buyScript = "";
+
             $scope.generateScript = function() {
                 $scope.buyScript = "bind \"q\" \"";
                 angular.forEach($scope.selectedBindnames, function(bindname){
