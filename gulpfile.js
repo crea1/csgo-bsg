ca3a702f077b3dd70e287578199ade5bb34a6045
Marius Kristensen <marius.kristensen@gmail.com>
2015-09-15 12:46:53 +0200

    Added clean step that always runs first

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,45 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
+var del = require('del');
 
 var BUILD_DIR = 'build/'
 
-gulp.task('js', function() {
+// Remove build folder
+gulp.task('clean', function() {
+    return del(['build/'])
+});
+
+// Minify javascripts
+gulp.task('js', ['clean'], function() {
     return gulp.src('js/csgobind.js')
         .pipe(uglify({ mangle:true }))
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest(BUILD_DIR + 'js/'));
 });
 
-gulp.task('html', function() {
+// Move html files and update references 
+gulp.task('html', ['clean'], function() {
     return gulp.src('index.html')
         .pipe(replace('js/csgobind.js', 'js/csgobind.min.js'))
         .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('styles', function() {
+// Move css files
+gulp.task('styles', ['clean'], function() {
     return gulp.src('css/*.css')
         .pipe(gulp.dest(BUILD_DIR + 'css/'));
 });
 
-gulp.task('data', function() {
+// Move datafiles
+gulp.task('data', ['clean'], function() {
     return gulp.src('csgoitems.json')
         .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('bower_components', function() {
+// Move bower components
+// TODO change to only grab relevant files
+gulp.task('bower_components', ['clean'], function() {
     var components = [   ];
     return gulp.src('bower_components/**')
         .pipe(gulp.dest(BUILD_DIR + 'bower_components/'));
